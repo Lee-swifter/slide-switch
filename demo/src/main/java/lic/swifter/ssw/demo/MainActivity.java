0c7f3d23193e7b2172c44b648b166009165a578d
Lee-swifter <chengkong2006@126.com>
2020-11-09 11:15:17 +0800

    update version and delete unnecessary gradle plugins

--- demo/src/main/java/lic/swifter/ssw/demo/MainActivity.java
+++ demo/src/main/java/lic/swifter/ssw/demo/MainActivity.java
@@ -1,12 +1,11 @@
 package lic.swifter.ssw.demo;
 
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import lic.swifter.ssw.SlideSwitch;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
